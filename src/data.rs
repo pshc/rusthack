9582bf9a2f5d3072a8c31d63072f91e4362c39ee
jaywuuu <jwu@nightingalemd.com>
2015-06-15 16:00:39 -0400

    quick addition of level bounds and bounds checking.

--- src/data.rs
+++ src/data.rs
@@ -7,14 +7,30 @@ impl Pos {
     }
 }
 
+pub struct Bounds {
+    pub x: u8,
+    pub y: u8,
+    pub max_x: u8,
+    pub max_y: u8,
+}
+
+impl Bounds {
+    pub fn new(x: u8, y: u8, max_x: u8, max_y: u8) -> Bounds {
+         Bounds {x: x, y: y, max_x: max_x, max_y: max_y }
+    }
+}
+
+
 pub struct Level {
     pub player_pos: Pos,
+    pub level_bounds: Bounds,
 }
 
 impl Level {
-    pub fn new() -> Level {
+    pub fn new(player_pos: Pos, level_bounds: Bounds) -> Level {
         Level {
-            player_pos: Pos::new(0, 0),
+            player_pos: player_pos,
+            level_bounds: level_bounds,
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,10 @@ fn step(level: &mut data::Level, io: &mut io::Io) -> bool {
 
     match key.unwrap() {
         Escape => return false,
-        Up => if level.player_pos.y > 0 { level.player_pos.y -= 1; },
-        Down => level.player_pos.y += 1,
-        Left => if level.player_pos.x > 0 { level.player_pos.x -= 1; },
-        Right => level.player_pos.x += 1,
+        Up => if level.player_pos.y > level.level_bounds.y { level.player_pos.y -= 1; },
+        Down => if level.player_pos.y < level.level_bounds.max_y { level.player_pos.y += 1 },
+        Left => if level.player_pos.x > level.level_bounds.x { level.player_pos.x -= 1; },
+        Right => if level.player_pos.x < level.level_bounds.max_x { level.player_pos.x += 1 },
     }
     true
 }
@@ -27,7 +27,10 @@ fn step(level: &mut data::Level, io: &mut io::Io) -> bool {
 fn main() {
     let mut io = io::Io::new();
 
-    let mut level = data::Level::new();
+    let mut level = data::Level::new(
+        data::Pos::new(0,0),
+        data::Bounds::new(0,0,79,49),
+        );
     while !io.window_closed() {
         render::render(&level, &mut io);
         if !step(&mut level, &mut io) {
